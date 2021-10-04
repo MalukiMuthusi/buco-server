2c100f81da8cd365e41fb150b01226f080413d3a
Maluki Muthusi <malukimuthusi@gmail.com>
2021-10-04 10:59:30 +0300

    Use .env files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3003;
+const port = process.env.PORT;
 const contact = require("./contact/contact");
 
 const urlEncodedParser = express.urlencoded({ extended: false });
